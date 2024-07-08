9122fb4457c4597fb565744b951c8f39be0191c3
Layse Prado <laysesprado@gmail.com>
2024-07-08 20:01:53 -0300

    Update - WorkoutAPI

--- workoutapi/workoutapi/atleta/schemas.py
+++ workoutapi/workoutapi/atleta/schemas.py
@@ -1,6 +1,9 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from pydantic import Field, PositiveFloat
-from workoutapi.contrib.schemas import BaseSchema
+from workoutapi.categorias.schemas import CategoriaIn
+from workoutapi.centro_treinamento.schemas import CentroTreinamentoAtleta
+
+from workoutapi.contrib.schemas import BaseSchema, OutMixin
 
 class Atleta(BaseSchema):
     nome: Annotated[str, Field(description='Nome do Atleta', example='Joao', max_length=50)]
@@ -9,3 +12,17 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description='Peso do Atleta', example=75.5)]
     altura: Annotated[PositiveFloat, Field(description='Altura do Atleta', example=1.70)]
     sexo: Annotated[str, Field(description='Sexo do Atleta', example='M', max_length=1)]
+    categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
+    centro_treinamento: Annotated[CentroTreinamentoAtleta, Field(description='Centro de treinamento do atleta')]
+
+
+class AtletaIn(Atleta):
+    pass
+
+
+class AtletaOut(Atleta, OutMixin):
+    pass
+
+class AtletaUpdate(BaseSchema):
+    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
+    idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
--- workoutapi/workoutapi/main.py
+++ workoutapi/workoutapi/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
+from workoutapi.routers import api_router
 
-app = FastAPI(title="WorkoutApi")
-
-if __name__ == 'main':
-    import uvicorn
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info', reload=True)
+app = FastAPI(title='WorkoutApi')
+app.include_router(api_router)
